9b0a0d0131781a0f140eec23df6497c92fa30f47
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-10 04:41:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,5 @@
 #write your code here
 
-def countdown(x)
-  count = x
-  while count < 0
-    puts "#{x} SECOND(S)!"
-    count += 1
-  end
-  puts "Happy New Year"
+def countdown(seconds)
+  
 end
